c55ef720e98462ed2c2ef6bb37b692e39358adef
MaaikeSchoute <maaike.schoute2@hva.nl>
2024-11-27 14:25:28 +0100

    fonts en banner en hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
 // JavaScript Document
 console.log("hi");
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+});
