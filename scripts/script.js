f79d54090b3cf872b02ffeec9ca93e0a5951e95e
MaaikeSchoute <maaike.schoute2@hva.nl>
2024-12-04 09:12:45 +0100

    meer dingen geprobeerd

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,2 @@
 // JavaScript Document
 console.log("hi");
-
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('open');
-});
